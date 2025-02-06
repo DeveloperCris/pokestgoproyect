6a4f76c571830b52bc603282402ed0dc33c84a3a
DeveloperCris <129326992+DeveloperCris@users.noreply.github.com>
2025-02-05 22:39:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,26 @@
     {
         name: "PIN Pokestgo",
         description: "Representa a una de las comunidades más grandes de latinoamerica y del mundo",
-        image: "/Images/pinps.png",
+        image: "Images/pinps.png",
         price: "$5.000"
     },
     {
         name: "PIN Safari City Santiago 2025",
         description: "El recuerdo perfecto de un evento único",
-        image: "/Images/pinsc.jpg",
+        image: "Images/pinsc.jpg",
         price: "$8.000"
 
     },
     {
         name: "PROXIMAMENTE Kit City Safari x Pokestgo",
         description: "Más detalles dentro de poco",
-        image: "/Images/soon.png",
+        image: "Images/soon.png",
         price: "$20.000"
     },
     {
         name: "PROXIMAMENTE Camiseta Pokestgo",
         description: "Más detalles dentro de poco",
-        image: "/Images/soon1.png",
+        image: "Images/soon1.png",
         price: "$20.000"
     }
 ];
